2b869af614d6fb09795bc5d858c7a95cca100fe4
HernanMartin <hernan.rodriguez.0824@gmail.com>
2023-12-24 11:54:20 -0300

    24/12/2023

--- src/app/todolist/todolist.component.ts
+++ src/app/todolist/todolist.component.ts
@@ -1,4 +1,4 @@
-import { Component,signal } from '@angular/core';
+import { Component,signal,EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
@@ -14,17 +14,19 @@ export class TodolistComponent {
   indice:number = -1;
   button:boolean = false;
   colors = signal([
-  'bg-pink-200',
-  'bg-blue-200',
-  'bg-green-200',
-  'bg-yellow-200',
-  'bg-purple-200',
-  'bg-orange-200',
-  'bg-indigo-200',
-  'bg-teal-200',
-  'bg-red-200',
-  'bg-cyan-200']);
-  colorChange:string = '';
+  'bg-pink-300',
+  'bg-blue-300',
+  'bg-green-300',
+  'bg-yellow-300',
+  'bg-purple-300',
+  'bg-orange-300',
+  'bg-indigo-300',
+  'bg-teal-300',
+  'bg-red-300',
+  'bg-cyan-300']);
+
+  colorChange:string = 'bg-EDB099';
+  @Output() colorSelected = new EventEmitter<string>();
 
   tasks = signal([{
       taskname: 'T1',
@@ -78,6 +80,7 @@ export class TodolistComponent {
       return(hours + ":" + minutes)
     }
   }
+
   openAndClose(){
       if(this.button){
         this.button = false;
@@ -86,6 +89,13 @@ export class TodolistComponent {
       }
       return(this.button)
   }
-  colorManager(){
+  colorManager(color:string){
+    this.colorChange = color
+    this.colorSelected.emit(color);
   }
+
+  getTextColor(){
+    return(this.colorChange)
+  }
+
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,32 @@ module.exports = {
   theme: {
     extend: {
       backgroundColor : {
-        "edb099" : '#EDB099',
+        "EDB099" : '#EDB099',
         "FDFAFA" : '#FDFAFA',
         "ED5555": '#ED5555',
         "E9E9E9" : '#E9E9E9'
-      },      
+      },
+            
       fontFamily: {
         'inter': ['Inter', 'sans-serif'],
       },
+      colors : {'edb099' : '#EDB099'},
 
-      colors : {'edb099' : '#EDB099'}
+      textShadow: {
+        'xs': '0 1px 3px rgba(0, 0, 0, 0.3)',
+        'sm': '0 1px 2px rgba(0, 0, 0, 0.3)',
+        'md': '0 4px 6px rgba(0, 0, 0, 0.3)',
+        'lg': '0 4px 12px rgba(0, 0, 0, 0.3)'
+        // ... otras sombras que desees agregar
+      },
+
+      textColor:{
+        "EDB099" : '#EDB099',
+        "FDFAFA" : '#FDFAFA',
+        "ED5555": '#ED5555',
+        "E9E9E9" : '#E9E9E9'
+      }
+      
     },
   },
   plugins: [],
