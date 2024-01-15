9473b8d0548e545771e9757ef87d3299bfd0927c
HernanMartin <hernan.rodriguez.0824@gmail.com>
2024-01-15 02:09:37 -0300

    a

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
-<<<<<<< HEAD
-
-export const routes: Routes = [
-    {path : '', component: LoginComponent},
-=======
 import { AnualViewComponent } from './components/Containers/anual-view/anual-view.component';
 
 export const routes: Routes = [
     {path: '', component: LoginComponent},
     {path: 'home', component : AnualViewComponent}
->>>>>>> 7daa813557439865b1d02607820b68c51e97f8cb
 ];
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,24 +1,14 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
-
-@Component({
-  selector: 'app-login',
-  standalone: true,
-  imports: [],
-=======
 import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router'; 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [ReactiveFormsModule],
->>>>>>> 7daa813557439865b1d02607820b68c51e97f8cb
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-<<<<<<< HEAD
-=======
   applyForm = new FormGroup({
     user : new FormControl(''),
     password : new FormControl(''),
@@ -28,11 +18,11 @@ export class LoginComponent {
 
   }
   
+  // Uso provisorio - implementar un metodo de autenticacion por tokens para inicio de sesion
   checkValue(){
     if(this.applyForm.value.user == 'hernan' && this.applyForm.value.password == '123'){
       this.router.navigate(['/home'])
     }
   }
->>>>>>> 7daa813557439865b1d02607820b68c51e97f8cb
 
 }
